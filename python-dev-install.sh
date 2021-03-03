ff0796d470105bf39f1bf7c5254ac03b846c8684
n3ar <jwarne90@gmail.com>
2021-03-03 10:22:21 -0500

    Python development configurations for spacevim added

--- python-dev-install.sh
+++ python-dev-install.sh
@@ -7,4 +7,9 @@ sudo apt install ipython3 -y
 
 # Python PIP and Packages
 sudo apt install python3-pip
-pip3 install pandas
+pip3 install --user pylint
+pip3 install --user yapf
+pip3 install --user autoflake
+pip3 install --user isort
+pip3 install --user coverage
+pip3 install --user pandas

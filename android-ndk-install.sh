8bd6f1dbac68fc99df1da23c240c009c34b0d212
n3ar <jwarne90@gmail.com>
2021-11-16 15:49:25 -0500

    Install scripts for android RE Tooling

---
+++ android-ndk-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "The sdkmanager method failed me because of some weird"
+echo "Java version issues that doesn't matter if you run the"
+echo "GUI: Open the GUI, create a project, and follow these instructions:"
+echo "https://developer.android.com/studio/projects/install-ndk#kts"
+
---
+++ jeb-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOME=/home/$(whoami)
+DWN=${HOME}/Downloads
+OPT=/opt
+TGT=${OPT}/jeb-ce
+
+ZIP="jeb-package.zip"
+URL="https://www.pnfsoftware.com/dl?jeb4ce"
+pushd ${DWN}
+ls | grep ${ZIP}
+if [[ $? -eq 1 ]]; then
+    wget -O ${ZIP} ${URL}
+fi
+
+ls ${OPT} | grep jeb-ce
+if [[ $? -eq 1 ]]; then
+    sudo mkdir ${TGT}
+    sudo unzip ${ZIP} -d ${TGT}
+    if [[ $? -ne 0 ]]; then
+        echo "Unzipping failed for some reason."
+        exit 1
+    fi
+fi
+
+sudo ln -s ${TGT}/jeb_linux.sh ${HOME}/.local/bin/jeb_ce
+
+rm ${ZIP}
+popd # DWN
+
+echo "jeb-ce install complete! Close your terminal and run 'jeb-ce'."

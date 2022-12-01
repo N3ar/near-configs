00e1e411ae04d60b63e6c8217f2423f5dd02f764
N3ar <jwarne90@gmail.com>
2022-12-01 09:01:07 -0500

    Update to remove telemetry

--- first.sh
+++ first.sh
@@ -3,4 +3,7 @@ echo "Updating apt repos to avoid broken links"
 sudo apt update -y
 sudo apt upgrade -y
 
+sudo apt purge -y apport
+sudo apt remove -y popularity-contest
+
 sudo apt install -y curl

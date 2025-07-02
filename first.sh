2158376b6f7553e0dae7d5a688cbc94865c6d2db
n3ar <jwarne90@gmail.com>
2025-07-02 18:27:23 -0400

    Require snap on ubuntu, fix p10k install

--- first.sh
+++ first.sh
@@ -3,6 +3,13 @@ echo "Updating apt repos to avoid broken links"
 sudo apt update -y
 sudo apt upgrade -y
 
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+if [[ ${DISTRO} == "ubuntu" ]]; then
+    echo "You're on Ubuntu, so I am installing Snap to keep it easy."
+    sudo apt install snapd -y
+    exit 0
+fi
+
 sudo apt purge -y apport
 sudo apt remove -y popularity-contest
 
--- p10k-install.sh
+++ p10k-install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "Installing Powerlevel10k...\n"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+p10k_tgt=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d "$p10k_tgt" ]; then
+	echo "Installing Powerlevel10k...\n"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${p10k_tgt}
+fi
 
 cp p10k-configs/.zshrc /home/$(whoami)/.zshrc
-source ~/.zshrc
 
-echo "If p10k configure hasn't run yet do it manually after closing all shells and opening a new one."
+echo "[+] Configs are in place! powerlevel10k will run the next time you execute zsh"

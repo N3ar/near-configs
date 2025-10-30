aada9feae679ae51f5185a8402cef8a993b73dfa
n3ar <jwarne90@gmail.com>
2025-10-29 21:12:36 -0400

    zsh install updated to include guix, corrected pathing for syntax-highlighting, and omz

--- zsh-install.sh
+++ zsh-install.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-echo "Installing zsh dependencies..."
-sudo apt install wget curl git -y
 
-echo "Installing zsh core..."
-sudo apt install zsh -y
+source ./HELPERS.sh
+source ./env.sh
+
+notify i "Installing zsh dependencies..."
+notify i "Installing zsh core..."
+if [[ $(is_installed guix) -gt 0 ]]; then
+    sudo apt install zsh wget curl git -y
+else
+    guix install zsh wget curl git
+fi
 
 echo "ZSH Installed successfully"
 zsh --version
 
 echo "Installing Oh-my-Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Configuring OMZ..."
 source ~/.zshrc
 
 echo "Installing Custom Fonts and Context Highlighting..."
-sudo apt install powerline fonts-powerline zsh-syntax-highlighting -y
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+if [[ $(is_installed guix) -gt 0 ]]; then
+    sudo apt install powerline fonts-powerline zsh-syntax-highlighting -y
+    echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+else
+    guix install zsh-syntax-highlighting
+    sudo apt install powerline fonts-powerline -y
+    echo "source $GUIX_PROFILE/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+fi
 
 echo "Setting ZSH as default shell"
 USER=$(whoami)

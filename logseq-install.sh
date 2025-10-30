6d24615a4c0e7f8f302a3f2d851dd0287f2daa4b
n3ar <jwarne90@gmail.com>
2025-10-30 14:10:00 -0400

    logseq install updated

--- logseq-install.sh
+++ logseq-install.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 
+source ./env.sh
+source ./HELPERS.sh
+
+if [[ $(is_installed flatpak) -eq 0 ]]; then
+    notify "Prioritizing flatpak installation"
+    flatpak install --user flathub com.logseq.Logseq
+    flatpak update
+
+    notify "Flatpak generally doesn't have filesystem access"
+    notify "Granting access to near's external notes location as of 20251030"
+    notify w "Granting access to $HOME/Documents/notes"
+    flatpak override --user com.logseq.Logseq --filesystem=$HOME/Documents/notes
+
+    notify "New accesses:"
+    flatpak info --show-permissions com.logseq.Logseq
+
+    notify s "Logseq installed via Flatpak"
+    exit 0
+fi
+
+notify "Flatpak not found, installing via AppImage"
+
 # Install Location
-TGT=/home/$(whoami)/.local/bin
+TGT=${HOME}/.local/bin
 mkdir -p ${TGT}
 
 # Get latest AppImage

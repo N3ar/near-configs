581abf3486ec5bdf9372470cd673177d7520b8b5
n3ar <jwarne90@gmail.com>
2025-10-31 09:59:10 -0400

    Neovim update to include nvchad

--- neovim-install.sh
+++ neovim-install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+source ./env.sh
+
 if command -v guix > /dev/null 2>&1; then
     # TODO Uncomment when python-setup is fixed or fix it myself when it is needed
     guix install neovim neovim-packer neovim-syntastic vim-vlime python-pynvim \
-    # python-neovim-remote 
+    # python-neovim-remote
+
+    git clone https://github.com/NvChad/starter ${HOME}/.config/nvim && nvim
 else
     sudo apt -y install neovim
+    git clone https://github.com/NvChad/starter ${HOME}/.config/nvim && nvim
 fi

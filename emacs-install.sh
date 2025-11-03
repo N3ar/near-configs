09f69e4db15721bc1f2cac319d7d9bbfd5e21415
n3ar <jwarne90@gmail.com>
2025-11-03 10:48:44 -0500

    emacs install updated to include bash-ls server requirements

--- emacs-install.sh
+++ emacs-install.sh
@@ -2,13 +2,24 @@
 
 set -euo pipefail
 
-# Step 1: Ensure Guix is available
-if ! command -v guix &> /dev/null; then
-    echo "Guix is not installed. Attempting to install guix."
-    echo "This may take a while....."
-    ./guix-install.sh
-    guix pull
+source ./HELPERS.sh
+
+if [[ $(is_installed guix) -gt 0 ]]; then
+    notify w "This system doesn't have guix."
+    notify "The lack of guix implies that it is not a primary system"
+    notify "Please install neovim and tmux instead"
+    notify e "This EMACS install isn't intended for terminal only use"
+    exit 1
 fi
+
+## Step 1: Ensure Guix is available
+## NOTE this is no longer necessary
+#if ! command -v guix &> /dev/null; then
+    #echo "Guix is not installed. Attempting to install guix."
+    #echo "This may take a while....."
+    #./guix-install.sh
+    #guix pull
+#fi
 
 # Step 2: Ensure ~/.guix-profile/bin is in PATH
 GUIX_BIN="$HOME/.guix-profile/bin"
@@ -31,6 +42,9 @@ guix install emacs-all-the-icons emacs-spaceline-all-the-icons \
 emacs-nerd-icons font-awesome font-google-material-design-icons \
 emacs-powerline emacs-spaceline
 
+# Install pandoc for markdown, latex, and other support
+guix install pandoc emacs-markdown-mode emacs-pandoc-mode emacs-ox-pandoc
+
 # Step 4: Create an optional symlink for convenience
 TARGET_BIN="$HOME/.guix-profile/bin/emacs"
 LINK_BIN="$HOME/.local/bin/emacs"
@@ -43,7 +57,17 @@ else
     echo "Symlink $LINK_BIN already exists."
 fi
 
+# TODO Switch to a near_alias
 echo "alias emacs='emacs > /dev/null 2>&1 & disown'" >> ${HOME}/.bash_aliases
+
+# TODO This path export needs to be in place to support npm things
+notify w "INSTALL NPM -- If you want to run bash-ls aka bash-language-server"
+cat <<EOF
+guix install node
+npm config set prefix "$HOME/.npm-global"
+export PATH="$HOME/.npm-global/bin:$PATH"
+npm install -g bash-language-server
+EOF
 
 echo "Done. Restart your shell or run: source $SHELL_RC"
 echo "Run 'emacs --version' to confirm installation."

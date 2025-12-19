f8f54b6382a01d5f68c93f27afe37871fd3c2fac
n3ar <jwarne90@gmail.com>
2025-12-19 15:55:32 -0500

    zsh update

--- zsh-install.sh
+++ zsh-install.sh
@@ -5,11 +5,12 @@ source ./env.sh
 
 notify i "Installing zsh dependencies..."
 notify i "Installing zsh core..."
-if [[ $(is_installed guix) -gt 0 ]]; then
-    sudo apt install zsh wget curl git -y
-else
-    guix install zsh wget curl git
-fi
+sudo apt install zsh wget curl git -y
+# if [[ $(is_installed guix) -gt 0 ]]; then
+#    sudo apt install zsh wget curl git -y
+#else
+#    guix install zsh wget curl git
+#fi
 
 echo "ZSH Installed successfully"
 zsh --version
@@ -24,10 +25,10 @@ echo "Installing Custom Fonts and Context Highlighting..."
 if [[ $(is_installed guix) -gt 0 ]]; then
     sudo apt install powerline fonts-powerline zsh-syntax-highlighting -y
     echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-else
-    guix install zsh-syntax-highlighting
-    sudo apt install powerline fonts-powerline -y
-    echo "source $GUIX_PROFILE/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#else
+#    guix install zsh-syntax-highlighting
+#    sudo apt install powerline fonts-powerline -y
+#    echo "source $GUIX_PROFILE/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 fi
 
 echo "Setting ZSH as default shell"
